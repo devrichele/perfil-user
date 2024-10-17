4a66d34be217e46644f36138de326558c69e0adf
Richele <168489035+devrichele@users.noreply.github.com>
2024-10-17 23:33:40 +0000

    criando projeto post para testar aprendizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import './index.css'; 
 
 function App() {
-
-
   return (
-    <di> Olá </di>
-  )
+    <div className="post">
+      <div className="body">
+        <img className='perfil'
+          src="https://media.licdn.com/dms/image/v2/D4D03AQH_z--8pl-nnA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1722022791055?e=1734566400&v=beta&t=8MxIeqQftTASwCElqIAG27ZqwLAEeJGQG-20JlLwCMU" 
+        />
+        <h2>Richele Br.</h2>
+      </div>
+      <p className='mensagem'> Olá pessoal, vamos codar?</p>
+      <img className='imgpost' src="https://www.dailyhostnews.com/wp-content/uploads/2023/08/React.js-Cross-Platform-Development.png"
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
